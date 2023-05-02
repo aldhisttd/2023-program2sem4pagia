f46fcee7ecd0d7e3164bf1353f1ecb74b4e9a4de
ismiati20 <ismiytismi2@gmail.com>
2023-05-02 16:25:15 +0700

    ini tugas 1 ismiati

---
+++ tugas1/2155201042.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrasi</title>
+</head>
+<body>
+    <form action="contact.php" method="POST">
+        <fieldset>
+        <legend>Registrasi</legend>
+        <p>
+            <label>Nama:</label>
+            <input type="text" name="nama" placeholder="Nama lengkap..." />
+        </p>
+        <p>
+            <label>Jenis kelamin:</label>
+            <label><input type="radio" name="jenis_kelamin" value="laki-laki" /> Laki-laki</label>
+            <label><input type="radio" name="jenis_kelamin" value="perempuan" /> Perempuan</label>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Send" />
+        </p>
+        </fieldset>
+    </form>
+</body>
+</html>
+<<<<<<< HEAD
+
+=======
+>>>>>>> c1e7601dd984fa154ad6f336eb01a4b063df484a
