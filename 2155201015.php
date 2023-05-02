1b70a6ef532a90a14826f703319e26d00061b42f
farhanariyandi <129134089+farhanariyandi@users.noreply.github.com>
2023-05-02 12:37:22 +0700

    Add files via upload

---
+++ 2155201015.php
@@ -0,0 +1,26 @@
+<form action="form input.php" method="POST">
+    <div>
+        <label>Nama</label> <br>
+        <input type="text" name="nama">
+    </div>
+    <div>
+        <label>Usia</label> <br>
+        <input type="number" name="usia">
+    </div>
+    <div>
+        <label>Tanggal Lahir</label> <br>
+        <input type="date" name="tanggal_lahir">
+    </div>
+    <div>
+        <label>Alamat</label> <br>
+        <textarea name="alamat"></textarea>
+    </div>
+    <div style="margin-bottom: 1rem;">
+        <label>Jenis Kelamin</label> <br>
+        <input type="radio" name="jenis_kelamin" value="l"> Laki-Laki <br>
+        <input type="radio" name="jenis_kelamin" value="p"> Perempuan
+    </div>
+    <div>
+        <button>Submit</button>
+    </div>
+</form>
