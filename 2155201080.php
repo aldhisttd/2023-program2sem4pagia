910f675a9c6174302bf641d315c0224a8169fab9
Agung Syahputra <agungsyhptr05@gmail.com>
2023-05-02 21:18:47 +0700

    form mahasiswa

---
+++ 2155201080.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Nama dan Tanggal Lahir</title>
+    <h1>Form Mahasiswa</h1>
+</head>
+<body>
+	<form>
+		<label for="nama">Nama:</label><br>
+		<input type="text" id="nama" name="nama"><br>
+		<label for="tanggal">Tanggal Lahir:</label><br>
+		<input type="date" id="tanggal" name="tanggal"><br><br>
+		<input type="submit" value="Submit">
+	</form> 
+</body>
+</html>
