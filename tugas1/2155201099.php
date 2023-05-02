f1bc033a13e7e44643f20f6165fac585b5bbe428
cahayadina <cahayanuzula17@gmail.com>
2023-05-02 17:35:14 +0700

    CAHAYADINA

---
+++ tugas1/2155201099.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Tugas Mahmud Alburozi</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<style>
+    *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    font-family: 'Open Sans', sans-serif;
+}
+body{
+    height: 100vh;
+    background-color:#E3F2C1;
+    background-size: cover;
+    background-position: center;
+    
+}
+h1{
+    text-align: center;
+    color: #3C486B;
+    margin-bottom: 30px;
+    text-transform: uppercase;
+    border-bottom: 4px solid #FDFEFE;
+}
+tr {
+    color: #394867;
+    margin-bottom: 30px;
+    background-position: center;
+}
+</style>
+    <body>
+        <h1 align="center">Data Pribadi</h3>
+        <table align="center"widht="600px">
+            <tr>
+                <td>Nama</td>
+                <td>: </td>
+                <td colspan="2"><input type="submit" value="Cahaya Dina Nuzula"></td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: </td>
+                <td colspan="2"><input type="submit" value="2155201099"></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="09 April 2003"></td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="Informatika"></td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="Islam"></td>
+            </tr>
+            </tr>
+        </table>
+        </div>     
+    </body>
+</html>
