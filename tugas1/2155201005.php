3a01cc2f5197855366c38251a9d7a64c70b6bdf8
santiprtwi <santipratiwi207@gmail.com>
2023-05-02 15:47:53 +0700

    DATA PRIBADI

---
+++ tugas1/2155201005.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Mahasiswa</title>
+    </head>
+    <body>
+
+        <h3 align="center">Data Mahasiswa</h3>
+        <hr width="600px">
+
+        <table align="center" cellpadding="10px" widht="600px">
+            <tr>
+                <td>Nama</td>
+                <td>: </td>
+                <td colspan="2"><input type="submit" value="Santi Pratiwi"></td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: </td>
+                <td colspan="2"><input type="submit" value="2155201005"></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="03 Februari 2003"></td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="Informatika"></td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="Islam"></td>
+            </tr>
+            <tr>
+                <td>Dosen Pengajar</td>
+                <td>:</td>
+                <td colspan="2"><input type="submit" value="Aldhi Albadri M.Kom"></td>
+            </tr>
+            <tr>
+            </tr>
+        </table>
+    </body>
+</html>
