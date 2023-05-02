cb578db9c75014da16e044bd8cff27e42ab28842
fazamazaya <fitritriee02@gmail.com>
2023-05-02 13:25:40 +0700

    Menambahkan form input mahasiswa

---
+++ tugas1/2155201030.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+   <title>Form Input Mahasiswa</title>
+</head>
+<body>
+   <form>
+      Nama: <input type="text" name="nama"><br>
+      Tanggal Lahir: <input type="date" name="tanggal_lahir"><br>
+      <input type="submit" value="Submit">
+   </form>
+</body>
+</html>
