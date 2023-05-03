30d257a9df96d26937b42e5b7767fa832d4e9cb7
riskynanda <132374260+riskynanda@users.noreply.github.com>
2023-05-03 14:01:46 +0700

    form sederhana

---
+++ 2155201016.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2> FORM MAHASISWA </h2>
+<form>
+  <label for="nama">Nama:</label><br>
+  <input type="text" id="nama" name="nama"><br>
+  <label for="tanggal">Tanggal Lahir:</label><br>
+  <input type="date" id="tanggal" name="tanggal">
+</form> 
+<input type="button" value="Submit" />
+</body>
+</html>
