991e8ff9421c5ffe63014a01358b2a0512c4b266
riskynanda <132374260+riskynanda@users.noreply.github.com>
2023-05-03 15:03:35 +0700

    form sederhana

--- 2155201101.php
+++ 2155201101.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html>
 <body>
-<<<<<<< HEAD
 <h3> Form Mahasiswa </h3>
 <form>
   <label for="nama">Nama:</label><br>
   <input type="text" id="nama" name="nama"><br>
   <label for="tanggal">Tanggal:</label><br>
-=======
-<h2> FORM MAHASISWA </h2>
-<form>
-  <label for="nama">Nama:</label><br>
-  <input type="text" id="nama" name="nama"><br>
-  <label for="tanggal">Tanggal Lahir:</label><br>
->>>>>>> 9e9bf7a5044c06c827ab77d34da78d18993c29cc
   <input type="date" id="tanggal" name="tanggal">
 </form> 
 <input type="button" value="Submit" />
