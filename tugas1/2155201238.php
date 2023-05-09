fc26930e41aa9d698c0bf4e494b1e63238c675c4
yyoosshhh <juliyosiyos@gmail.com>
2023-05-09 15:38:46 +0700

    Tugas Juliyos

---
+++ tugas1/2155201238.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input Mahasiswa</title>
+</head>
+<body>
+    <h1>Input Informasi Mahasiswa</h1>
+    <table>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                <td><input type="date"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="Simpan"></td>
+            </tr>
+        </table>
+</body>
+</html>
