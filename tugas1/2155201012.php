6ee59f61e4bcb7200d8c8504a2cc6f286b0b321f
Yutagaru <129131764+Yutagaru@users.noreply.github.com>
2023-05-10 14:16:18 +0700

    Update 2155201012.php

--- tugas1/2155201012.php
+++ tugas1/2155201012.php
@@ -2,8 +2,6 @@
     <head>
         <title>Mahasiswa</title>
     </head>
-    <body>
-        <h3>Data Mahasiswa</h3>
         <table>
             <tr>
                 <td>Nama</td>
@@ -11,17 +9,9 @@
                 <td><input type="text"></td>
             </tr>
             <tr>
-                <td>NIM</td>
-                <td>:</td>
-                <td><input type="text"></td>
-            </tr>
-            <tr>
                 <td>Tanggal Lahir</td>
                 <td>:</td>
                 <td><input type="date"></td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="Simpan"></td>
             </tr>
         </table>
     </body>
