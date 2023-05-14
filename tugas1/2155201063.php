4df5f38309fc3955c171966adaf610350ec89d09
D-lemillion <dyanafrnd@gmail.com>
2023-05-15 02:11:55 +0700

    REVISI PR DIAN AFRIANDI

---
+++ tugas1/2155201063.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title>MENGISI DATA MAHASISWA</title>
+</head><l
+<body>
+<div class="data">
+    <h3 class="data-header">FORUM DATA MAHASISWA</h3>
+        <form class="data-container">
+            <p>
+                <input type="NAMA" placeholder="NAMA">
+                <input type="NIM" placeholder="NIM">
+            </p>
+            <p>
+                <input type="NO.HP" placeholder="NO.HP">
+            </p>
+            <tr>
+                <td>Tanggal Lahir</td>
+                <td>:</td>
+                <td><input type="date"></td>
+            </tr>
+        </form>
+        <tr>
+            <td colspan="2"><input type="submit" value="Simpan"></td>
+        </tr>
+</div>
+</body>
+</html>
