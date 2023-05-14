81ee7f23cd0d432c06c5e9883355050ab5d39587
D-lemillion <dyanafrnd@gmail.com>
2023-05-15 01:39:09 +0700

    REVISI PR AKHIR DIAN AFRIANDI

---
+++ tugas1/2155201063.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>MAHASISWA</title>
+</head><l
+<body>
+<div class="login">
+    <h3 class="login-header">MAHASISWA</h2>
+        <form class="login-container">
+            <p>
+                <input type="NAMA" placeholder="NAMA">
+                <input type="NIM" placeholder="NIM">
+            </p>
+            <table>
+                <tr>
+                    <td>Tanggal Lahir</td>
+                    <td>:</td>
+                    <td><input type="date"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Simpan"></td>
+                </tr>
+            </table>
+        </form>
+</div>
+</body>
+</html>
