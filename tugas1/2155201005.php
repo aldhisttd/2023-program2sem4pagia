960dda8ea916dad041a3f08bbd5bdfe881730412
santiprtwi <129132295+santiprtwi@users.noreply.github.com>
2023-05-15 11:22:20 +0700

    Update 2155201005.php
    menghapus nim, jurusan, dan nama dosen pengajar

--- tugas1/2155201005.php
+++ tugas1/2155201005.php
@@ -14,32 +14,11 @@
                 <td colspan="2"><input type="submit" value="Santi Pratiwi"></td>
             </tr>
             <tr>
-                <td>NIM</td>
-                <td>: </td>
-                <td colspan="2"><input type="submit" value="2155201005"></td>
-            </tr>
-            <tr>
                 <td>Tanggal Lahir</td>
                 <td>:</td>
                 <td colspan="2"><input type="submit" value="03 Februari 2003"></td>
             </tr>
-            <tr>
-                <td>Jurusan</td>
-                <td>:</td>
-                <td colspan="2"><input type="submit" value="Informatika"></td>
-            </tr>
-            <tr>
-                <td>Agama</td>
-                <td>:</td>
-                <td colspan="2"><input type="submit" value="Islam"></td>
-            </tr>
-            <tr>
-                <td>Dosen Pengajar</td>
-                <td>:</td>
-                <td colspan="2"><input type="submit" value="Aldhi Albadri M.Kom"></td>
-            </tr>
-            <tr>
-            </tr>
+     
         </table>
     </body>
 </html>
