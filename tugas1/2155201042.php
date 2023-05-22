02a684d460d83743a5aae689f4a1c96f685d6528
ridiahayuvitaloka <ridiahayuvitaloka2216@gmail.com>
2023-05-22 09:48:34 +0700

    Hallo saya ridiah

---
+++ tugas1/2155201042.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>mahasiswa</title>
+</head>
+<body>
+    <h3>Data mahasiswa</title>
+        <table>
+             <tr>
+                 <td>nama</td>
+                 <td>:</td>
+                 <td><input type="text"
+></td>
+             </tr>
+             <tr>
+                 <td>tanggal lahir</td>
+                 <td>:</td>
+                 <td><input type="date"
+></td>
+            </tr>
+    </table>
+    </body>
+</html>
+=======
+>>>>>>> c1e7601dd984fa154ad6f336eb01a4b063df484a
