f93834533c6c53da4962dd806d684d7f557d3005
dheaamanda12 <yesayasimanjuntak17@gmail.com>
2023-05-27 20:52:48 +0700

    upload file dan form dan proses

--- tugas2/2155201045/index.php
+++ tugas2/2155201045/index.php
@@ -30,7 +30,7 @@
         <td><input type="file" name="foto" accept="images/*"></td>
     </tr>
     <tr>
-        <td colspan="2"><input type="submit" name="spn" value="simpan"></td>
+        <td colspan="2"><input type="submit" name="spn" value="submit"></td>
     </tr>
 </table>
     </form>
