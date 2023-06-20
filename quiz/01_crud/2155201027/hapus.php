23b2104eb74b4b82ae07e14c3b58f0321246ab40
NaniMaulana04 <nanimaulana74@gmail.com>
2023-06-20 10:18:21 +0700

    quiz nani

--- quiz/01_crud/2155201027/hapus.php
+++ quiz/01_crud/2155201027/hapus.php
@@ -3,11 +3,11 @@
 echo "menghapus data";
 
 // koneksi ke db
-$koneksi = mysqli_connect('127.0.0.1','root','root','4pagia','3307');
+$koneksi = mysqli_connect('localhost','root','','data_barang');
 
 // jalankan query
-$kode = $_REQUEST['kd_barang'];
-mysqli_query($koneksi, "DELETE FROM barang WHERE kode='$kd_barang'");
+$kode = $_REQUEST['kd_brg'];
+mysqli_query($koneksi, "DELETE FROM barang WHERE kode='$kd_brg'");
 
 // redirect ke table
 header("location:index.php");
