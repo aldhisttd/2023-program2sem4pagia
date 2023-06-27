f3af45d43b6b997621cb51dabe4bfc24c81d95ff
atika06 <atikajamil0691@gmail.com>
2023-06-27 09:43:58 +0700

    quiz login

---
+++ quiz/02_login/2155201008/user.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION['sudah_login'])){
+    header('location:form.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman User</title>
+</head>
+<body>
+    <p>Login sbg : <?php echo $_SESSION['username'] ?></p>
+    <p><a href="logout.php">Logout</a></p>
+    <h1>Selamat Datang di Halaman User</h1>
+</body>
+</html>
