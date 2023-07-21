058e016bb3cc7393f70300f957ce1aac92df6264
D-lemillion <dyanafrnd@gmail.com>
2023-07-21 16:14:49 +0700

    logindb dianafriandi

---
+++ quiz/05_logindb/2155201063/role/admin.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['sudah_login'])) {
+    header('location: ../form.php');
+    exit();
+}
+
+// Periksa peran pengguna
+// Periksa peran pengguna
+if ($_SESSION['username'] === 'admin') {
+    // User adalah admin, lanjutkan ke halaman admin
+    // ...
+} elseif ($_SESSION['username'] === 'user') {
+    // User adalah user, lanjutkan ke halaman user
+    header('location: user.php');
+    exit();
+} else {
+    // User bukan admin atau user yang valid
+    header('location: karyawan.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body style="background-color:#C4D7B2;">
+    <p>Login sbg : <?php echo $_SESSION['username'] ?></p>
+    <h1 style="color:#1D267D">Selamat Datang di Halaman Admin</h1>
+    <p><a href="../act/logout.php">Logout</a></p>
+</body>
+</html>
