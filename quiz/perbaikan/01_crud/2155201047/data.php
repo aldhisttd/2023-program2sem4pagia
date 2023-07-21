b5c6585f5e862f1dc747703f13fcca5e6f4ce82f
Anisa67 <rahmawatianissa307@gmail.com>
2023-07-21 21:15:23 +0700

    Perbaikan crud saya pak annisarahmawati

---
+++ quiz/perbaikan/01_crud/2155201047/data.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td,
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include "component/menu.php" ?>
+  <h2>Data Mahasiswa</h2>
+
+  <table>
+    <tr>
+      <th>NO</th>
+      <th>Nim</th>
+      <th>Nama</th>
+      <th>Jurusan</th>  
+      <th>Umur</th>
+	    <th>Photo</th>
+      <th>Aksi</th>
+    </tr>
+    <?php
+    include "action/koneksi.php";
+    $data = mysqli_query($koneksi, "SELECT * FROM perbaikan_crud");
+    $no = 1;
+    while ($row = mysqli_fetch_array($data)) {
+    ?>
+      <tr>
+        <td><?php echo $no ?></td>
+        <td><?php echo $row['nim'] ?></td>
+        <td><?php echo $row['nama'] ?></td>
+        <td><?php echo $row['jurusan'] ?></td>
+        <td><?php echo $row['umur'] ?></td>
+        <td>
+          <img src="uploads/<?php echo $row['foto'] ?>" width="250px" height="250px">
+        </td>
+        <td>
+          <a href="edit.php?nim=<?php echo $row['nim'] ?>">Edit</a> 
+          <a href="hapus.php?nim=<?php echo $row['nim'] ?>">Hapus</a>
+        </td>
+      </tr>
+    <?php
+    $no++;
+    }
+    ?>
+
+  </table>
+
+</body>
+
+</html>
