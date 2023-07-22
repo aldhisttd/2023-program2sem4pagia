e08b941cca432d6141bd2460ab7a8b508996f13c
diansefiani12 <diansefiani23@gmail.com>
2023-07-22 10:15:22 +0700

    logindb

---
+++ quiz/05_logindb/2155201028/act/role/user.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['sudah_login'])) {
+    header('location: ../form.php');
+    exit();
+}
+
+// Periksa peran pengguna
+if ($_SESSION['username'] === 'user') {
+    // User adalah admin, lanjutkan ke halaman admin
+    // ...
+} elseif ($_SESSION['username'] === 'admin') {
+    // User adalah user, lanjutkan ke halaman user
+    header('location: admin.php');
+    exit();
+} else {
+    // User bukan admin atau user yang valid
+    header('location: karyawan.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman User</title>
+</head>
+<body>
+    <p>Login sbg : <?php echo $_SESSION['username'] ?></p>
+    <p><a href="../act/logout.php">Logout</a></p>
+    <h1>Selamat Datang di Halaman User</h1>
+</body>
+</html>
